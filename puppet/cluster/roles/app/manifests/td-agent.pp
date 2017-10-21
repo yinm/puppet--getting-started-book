63dea58599d2f7763dd418765f772ae8bb2e6d78
yinm <yinm.cminor9@gmail.com>
2017-10-21 19:07:02 +0900

    Fix typo

--- puppet/cluster/roles/app/manifests/td-agent.pp
+++ puppet/cluster/roles/app/manifests/td-agent.pp
@@ -2,7 +2,7 @@ class app::td-agent {
   include ::td-agent
   include app::td-agent::config
 
-     Class['::td-agent:;install']
+     Class['::td-agent::install']
   -> Class['app::td-agent::config']
   ~> Class['::td-agent::service']
 }
